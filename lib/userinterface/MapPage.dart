6a01f13a9660130a5bbcf82ae71b830c9d1d0f66
Fabian <fabian_leibinger@gmx.de>
2021-08-14 18:41:31 +0200

    added shadow to textfield

--- lib/userinterface/MapPage.dart
+++ lib/userinterface/MapPage.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
@@ -8,6 +7,8 @@ import 'package:travel_information_app/models/global.dart';
 
 class MapPage extends StatefulWidget {
   MapPage({Key key}) : super(key: key);
+
+  final double borderRadius = 30;
 
   @override
   _MapPageState createState() => _MapPageState();
@@ -46,25 +47,34 @@ class _MapPageState extends State<MapPage> {
             ),
             Align(
               alignment: Alignment(0, -0.75),
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(25, 7, 25, 10),
-                child: Container(
-                  height: 45,
-                  child: TextField(
-                    autocorrect: false,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search),
-                      labelText: "search location",
-                      filled: true,
-                      fillColor: white,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30)),
-                          borderSide: BorderSide.none),
-                    ),
+              child: Container(
+                margin: EdgeInsets.fromLTRB(25, 7, 25, 10),
+                height: 45,
+                decoration: BoxDecoration(
+                  borderRadius:
+                      BorderRadius.all(Radius.circular(widget.borderRadius)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: grey.withOpacity(0.4),
+                      blurRadius: 3,
+                    )
+                  ],
+                ),
+                child: TextField(
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    labelText: "search location",
+                    filled: true,
+                    fillColor: white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                            Radius.circular(widget.borderRadius)),
+                        borderSide: BorderSide.none),
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ));
   }
