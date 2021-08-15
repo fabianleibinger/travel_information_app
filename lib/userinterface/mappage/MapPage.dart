bdcd2a5cc5a223f168c96a6f3ab607ecff6934a8
Fabian <fabian_leibinger@gmx.de>
2021-08-15 12:41:13 +0200

    added slidinguppanel

--- lib/userinterface/mappage/MapPage.dart
+++ lib/userinterface/mappage/MapPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:travel_information_app/userinterface/drawer/AppDrawer.dart';
 import 'package:travel_information_app/userinterface/mappage/LocationSearchField.dart';
 
@@ -18,41 +19,55 @@ class _MapPageState extends State<MapPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: AppDrawer(),
-      body: Stack(
-        children: [
-          FlutterMap(
-            options: MapOptions(
-              center: LatLng(51.5, -0.09),
-              zoom: 13.0,
-            ),
-            layers: [
-              TileLayerOptions(
-                  urlTemplate:
-                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  subdomains: ['a', 'b', 'c']),
-              MarkerLayerOptions(
-                markers: [
-                  Marker(
-                    width: 80.0,
-                    height: 80.0,
-                    point: LatLng(51.5, -0.09),
-                    builder: (ctx) => Container(
-                      child: FlutterLogo(),
+      body: SlidingUpPanel(
+        minHeight: 25,
+        backdropEnabled: true,
+        panel: Column(
+          children: [
+            Icon(Icons.keyboard_arrow_up_rounded),
+            Expanded(
+              child: Center(
+                child: Text("this is a sliding panel."),
+              ),
+            )
+          ],
+        ),
+        body: Stack(
+          children: [
+            FlutterMap(
+              options: MapOptions(
+                center: LatLng(51.5, -0.09),
+                zoom: 13.0,
+              ),
+              layers: [
+                TileLayerOptions(
+                    urlTemplate:
+                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    subdomains: ['a', 'b', 'c']),
+                MarkerLayerOptions(
+                  markers: [
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: LatLng(51.5, -0.09),
+                      builder: (ctx) => Container(
+                        child: FlutterLogo(),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                height: MediaQuery.of(context).padding.top,
-              ),
-              LocationSearchField(),
-            ],
-          ),
-        ],
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).padding.top,
+                ),
+                LocationSearchField(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
