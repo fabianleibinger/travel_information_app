058cf107b48d85ccd34a22e22bff6699ada1e884
Fabian <fabian_leibinger@gmx.de>
2021-08-17 09:22:03 +0200

    added user singleton for getting access to backend service

---
+++ lib/models/user/User.dart
@@ -0,0 +1,13 @@
+import 'package:travel_information_app/models/preferenceservice/user/login/AccessToken.dart';
+
+/// Singleton class for the current user of the app.
+class User {
+  static final User _instance = User._internal();
+
+  factory User() => _instance;
+
+  User._internal();
+
+  /// Access token for the backend service.
+  AccessToken? _accessToken;
+}
